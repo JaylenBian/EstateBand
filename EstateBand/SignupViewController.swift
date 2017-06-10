8befa681a49a1ec0a3b621eb88c772ff3ea8ba23
JunLin Bian <Minecoder@163.com>
2017-06-11 04:00:21 +0800

    The frame of this project

---
+++ EstateBand/SignupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignupViewController.swift
+//  EstateBand
+//
+//  Created by Minecode on 2017/6/11.
+//  Copyright © 2017年 org.minecode. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
---
+++ EstateBand/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  EstateBand
+//
+//  Created by Minecode on 2017/6/11.
+//  Copyright © 2017年 org.minecode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Widget Set
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signupAction(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
