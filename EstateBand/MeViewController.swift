3468df6c9c61cbeb5c3163eaeabf6c0c8a4306b4
JunLin Bian <Minecoder@163.com>
2017-06-27 17:56:18 +0800

    Upload user interface

---
+++ EstateBand/MeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MeViewController.swift
+//  EstateBand
+//
+//  Created by Minecode on 2017/6/27.
+//  Copyright © 2017年 org.minecode. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MeViewController: UIViewController {
+    
+    // 控件
+    @IBOutlet weak var mapKit: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.mapKit.mapType = .standard
+        let latDelta = 0.002
+        let longDelta = 0.002
+        let currentLocationSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        let locationManager:CLLocationManager = CLLocationManager()
+//        var center = locationManager.location?.coordinate
+        let center = CLLocationCoordinate2D(latitude: 29.596266757471625, longitude: 106.29760086536407)
+        let currentRegion: MKCoordinateRegion = MKCoordinateRegion(center: center, span: currentLocationSpan)
+        self.mapKit.setRegion(currentRegion, animated: true)
+        
+        var objectAnnotation = MKPointAnnotation()
+        objectAnnotation.coordinate = CLLocation(latitude: (center.latitude)+0.000010,
+                                                 longitude: (center.longitude)+0.000009).coordinate
+        objectAnnotation.title = "Danger Zone"
+        objectAnnotation.subtitle = "Tag by Security Guard NO.2"
+        self.mapKit.addAnnotation(objectAnnotation)
+        objectAnnotation = MKPointAnnotation()
+        objectAnnotation.coordinate = CLLocation(latitude: (center.latitude)-0.00013,
+                                                 longitude: (center.longitude)-0.00019).coordinate
+        objectAnnotation.title = "Danger Zone"
+        objectAnnotation.subtitle = "Tag by Security Guard NO.1"
+        self.mapKit.addAnnotation(objectAnnotation)
+        objectAnnotation = MKPointAnnotation()
+        objectAnnotation.coordinate = CLLocation(latitude: (center.latitude)+0.00033,
+                                                 longitude: (center.longitude)-0.00029).coordinate
+        objectAnnotation.title = "Danger Zone"
+        objectAnnotation.subtitle = "Tag by Security Guard NO.1"
+        self.mapKit.addAnnotation(objectAnnotation)
+    }
+
+}
